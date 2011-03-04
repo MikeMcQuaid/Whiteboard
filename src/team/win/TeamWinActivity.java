352c1f111c447d6c25ed83336ae5807b4faccd7e
Mike McQuaid <mike@mikemcquaid.com>
2011-03-04 18:38:41 +0000

    Initial Android import.

---
+++ src/team/win/TeamWinActivity.java
@@ -0,0 +1,13 @@
+package team.win;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeamWinActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
