11218fb492d618f83f96169e2258d11c5f1b75ea
Chris Paton <chris@chrispaton.co.uk>
2011-03-05 18:29:29 +0000

    Update width button to popup an AlertDialog to choose thickness.

--- src/team/win/WhiteBoardActivity.java
+++ src/team/win/WhiteBoardActivity.java
@@ -3,10 +3,14 @@
  */
 package team.win;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
@@ -20,6 +24,8 @@ import android.view.WindowManager;
 
 public class WhiteBoardActivity extends Activity {
 	
+	private static final int STROKE_WIDTH_DIALOG_ID = 1;
+
 	private static final String TAG = "WhiteBoardActivity";
 
 	private DataStore mDataStore = new DataStore();
@@ -29,16 +35,33 @@ public class WhiteBoardActivity extends Activity {
 	private Random mRandomSource = new Random();
 
 	private enum StrokeWidth {
-		SMALL(5),
-		MEDIUM(10),
-		LARGE(15);
-		int mWidth;
-		StrokeWidth(int width) {
+		NARROW(5, "Narrow"),
+		NORMAL(10, "Medium"),
+		THICK(15, "Thick"),
+		;
+		
+		static final String[] AS_STRINGS = new String[values().length];
+		static {
+			StrokeWidth[] values = values();
+			for (int i = 0; i < AS_STRINGS.length; i++) {
+				AS_STRINGS[i] = values[i].mDisplayName;
+			}
+		}
+		
+		final int mWidth;
+		final String mDisplayName;
+		
+		StrokeWidth(int width, String displayName) {
 			mWidth = width;
+			mDisplayName = displayName;
+		}
+		
+		static int indexOf(StrokeWidth strokeWidth) {
+			return Arrays.binarySearch(values(), strokeWidth);
 		}
 	};
 	
-	private StrokeWidth mLastWidth = StrokeWidth.SMALL;
+	private StrokeWidth mLastWidth = StrokeWidth.NORMAL;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -70,18 +93,7 @@ public class WhiteBoardActivity extends Activity {
 		case R.id.menu_save:
 			return true;
 		case R.id.menu_stroke_width:
-			switch(mLastWidth) {
-			case SMALL:
-				mLastWidth = StrokeWidth.MEDIUM;
-				break;
-			case MEDIUM:
-				mLastWidth = StrokeWidth.LARGE;
-				break;
-			case LARGE:
-				mLastWidth = StrokeWidth.SMALL;
-				break;
-			}
-			mWhiteBoardView.setPrimStrokeWidth(mLastWidth.mWidth);
+			showDialog(STROKE_WIDTH_DIALOG_ID);
 			return true;
 		case R.id.menu_color:
 			mWhiteBoardView.setPrimColor(
@@ -92,6 +104,27 @@ public class WhiteBoardActivity extends Activity {
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case STROKE_WIDTH_DIALOG_ID:
+			final CharSequence[] items = StrokeWidth.AS_STRINGS;
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Line thickness");
+			builder.setSingleChoiceItems(items, StrokeWidth.indexOf(mLastWidth), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int item) {
+					mLastWidth = StrokeWidth.values()[item];
+					mWhiteBoardView.setPrimStrokeWidth(mLastWidth.mWidth);
+					dialog.dismiss();
+				}
+			});
+			return builder.create();
+		default:
+			return super.onCreateDialog(id);
 		}
 	}
 
