5f6df0feb4fb82b32c18c4dafc7571ba86fa7572
Alistair John Strachan <alistair@devzero.co.uk>
2011-03-05 15:25:24 +0000

    Build fix

--- src/team/win/TeamWinActivity.java
+++ src/team/win/TeamWinActivity.java
@@ -11,6 +11,7 @@ import java.util.Enumeration;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
