eea5d8da00a95dbee530f8daccbacc070ca9d2f2
Charles Harley <charles.harley@gmail.com>
2011-03-05 12:12:47 +0000

    - Added a menu to the main screen. - Tidied up the manifest and strings file.

--- src/team/win/TeamWinActivity.java
+++ src/team/win/TeamWinActivity.java
@@ -8,8 +8,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TeamWinActivity extends Activity {
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,28 @@ public class TeamWinActivity extends Activity {
 		setContentView(R.layout.main);
 		startService(makeServiceIntent());
 		logIpAddresses();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_shutdown:
+			// TODO We need to properly shutdown the HTTP server.
+			// We want to allow the user to switch to other applications
+			// whilst the whiteboard is running and still give the user the ability to
+			// explicitly shutdown the application and stop the web server.
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	@Override
@@ -46,4 +72,5 @@ public class TeamWinActivity extends Activity {
 			throw new RuntimeException(e);
 		}
 	}
+	
 }
